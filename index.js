1c1b1d63c1db098243c30aa48c48d19065147bea
Arun Sagar S A <arunsagarsa99@gmail.com>
2024-08-25 19:37:39 -0400

    contians funcitons
    avoids adding extra code in html

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function takeMeToGithub() {
+  window.open("https://github.com/arun-sagar-s-a", "_blank");
+}
+function takeMeTolinkedIn() {
+  window.open(
+    "https://www.linkedin.com/in/arun-sagar-s-a-b70b1825b/",
+    "_blank"
+  );
+}
+function getMyResume() {
+  window.open("./assets/Resume-ArunSagarSA.pdf", "_blank");
+}
